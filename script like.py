f2dca3cf1b95c8c17f0030ee85fd511d2f751693
√(noham)² <100566912+NohamR@users.noreply.github.com>
2023-04-07 17:00:53 +0200

    belin

---
+++ script like.py
@@ -0,0 +1,4 @@
+# https://exobank.hachette-livre.fr/contents/final/9782017102106-fxl/OEBPS/Page_1.html
+
+for i in range (1,417):
+    print("""https://exobank.hachette-livre.fr/contents/final/9782017102106-fxl/OEBPS/Page_""",i,""".html""",sep='')
