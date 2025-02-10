ddc22ff6fa1accacfeb1c26ba9a14d71369bacd8
Wlofy <grosthug91@gmail.com>
2025-02-10 04:23:26 +0100

    epico

--- index.js
+++ index.js
@@ -19,12 +19,13 @@ function hub() {
     Swal.fire({
         title: "Choisissez un manuel",
         html: `
-        <button style="background-color:#E73E07" class="swal2-styled swal2-default-outline" onclick="changerURLOut('https://www.calameo.com/read/003221622c5e0d0b70eac?authid=Mgr3I6549pUc', 'Calameo')"><h1>Géographie</h1></button>
-		<button style="background-color:#37952E" class="swal2-styled swal2-default-outline" onclick="changerURLOut('https://www.calameo.com/read/00322162234bd5e552635?authid=nadWurkhaHUX', 'Calameo')"><h1>Histoire</h1></button>
-		<button style="background-color:#69C6AB" class="swal2-styled swal2-default-outline" onclick="changerURLOut('https://www.libmanuels.fr/demo/9782210114302/specimen/2/', 'Libmanuels')"><h1>EMC</h1></button>
-		<button style="background-color:#6CC247" class="swal2-styled swal2-default-outline" onclick="changerURLOut('https://www.calameo.com/read/004822953b9519a03ed95', 'Calameo')"><h1>Maths Exp.</h1></button>
-		<button style="background-color:#E94F45" class="swal2-styled swal2-default-outline" onclick="changerURLOut('https://www.calameo.com/read/0048229533c3f405328b4', 'Calameo')"><h1>Maths Spé.</h1></button>
-		<button style="background-color:#E3002B" class="swal2-styled swal2-default-outline" onclick="changerURLOut('https://www.calameo.com/read/003221622c2a061e54fab?authid=8JEKdft78E9d', 'Calameo')"><h1>HGGSP</h1></button>
+        <button style="background-color:#E73E07" class="swal2-styled swal2-default-outline" onclick="changerURLOut('https://www.calameo.com/read/003221622c5e0d0b70eac?authid=Mgr3I6549pUc')"><h1>Géographie</h1></button>
+		<button style="background-color:#37952E" class="swal2-styled swal2-default-outline" onclick="changerURLOut('https://www.calameo.com/read/00322162234bd5e552635?authid=nadWurkhaHUX')"><h1>Histoire</h1></button>
+		<button style="background-color:#69C6AB" class="swal2-styled swal2-default-outline" onclick="changerURLOut('https://www.libmanuels.fr/demo/9782210114302/specimen/2/')"><h1>EMC</h1></button>
+		<button style="background-color:#6CC247" class="swal2-styled swal2-default-outline" onclick="changerURLOut('https://www.calameo.com/read/004822953b9519a03ed95')"><h1>Maths Exp.</h1></button>
+		<button style="background-color:#E94F45" class="swal2-styled swal2-default-outline" onclick="changerURLOut('https://www.calameo.com/read/0048229533c3f405328b4')"><h1>Maths Spé.</h1></button>
+		<button style="background-color:#E3002B" class="swal2-styled swal2-default-outline" onclick="changerURLOut('https://www.calameo.com/read/003221622c2a061e54fab?authid=8JEKdft78E9d')"><h1>HGGSP</h1></button>
+		<button style="background-color:#E3002B" class="swal2-styled swal2-default-outline" onclick="changerURLOut('manuels/PC-Bordas.pdf')"><h1>Physique-Chimie</h1></button>
         `,
         icon: "question",
         confirmButtonColor: "#3085d6",
@@ -45,26 +46,12 @@ function hub() {
 }
 
 function changerURLOut(url, site) {
-    Swal.fire({
-        title: "Site externe",
-        html: `
-        Vous vous apprêtez à vous rendre sur <b>${site}</b>.
-        `,
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        cancelButtonText: "Annuler",
-        confirmButtonText: `
-        <i class="fa fa-external-link"></i> Continuer
-        `
-    }).then((result) => {
-        if (result.isConfirmed) {
-            window.location = url;
-        } else {
-            hub();
-        }
-    });
+    document.getElementById("manueliframecontainer").style.display = '';
+    const iframe = document.getElementById("manueliframe");
+    iframe.src = url;
+    Swal.close();
+    iframe.focus();
+    iframe.scrollIntoView();
 }
 
 function editionchoisie() {
